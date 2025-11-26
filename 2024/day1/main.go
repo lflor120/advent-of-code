2481bc06d632ba3abc508acc0aafe8ded94f654a
lflor120 <leonard4flores@gmail.com>
2025-11-25 22:20:36 -0600

    warming up w 2024 questions

---
+++ 2024/day1/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"log"
+	"strings"
+	"strconv"
+	"sort"
+	"math"
+)
+
+func main() {
+	fmt.Println("Day 1!")
+	file, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal("failed to open file!")
+	}
+
+	lines := strings.Split(string(file), "\n")
+	firstSlice, secondSlice := convertToSortedIntSlice(lines)
+	
+	solutionOne := getDistances(firstSlice, secondSlice)
+	fmt.Printf("Solution one: %d \n", solutionOne)
+
+	solutionTwo := getSimilarityScore(firstSlice, secondSlice)
+	fmt.Printf("Solution two: %d", solutionTwo)
+}
+
+func convertToSortedIntSlice(lines []string) ([]int, []int) {
+	firstSlice := make([]int, 0)
+	secondSlice := make([]int, 0)
+
+	for _, line := range lines {
+		nums := strings.Fields(line)
+		firstNum, err := strconv.Atoi(nums[0])
+		if err != nil {
+			log.Fatalf("Failed to proccess first number: %v", err)
+		}
+
+		secondNum, err := strconv.Atoi(nums[1])
+				if err != nil {
+			log.Fatalf("Failed to proccess second number: %v", err)
+		}
+
+		firstSlice = append(firstSlice, firstNum)
+		secondSlice = append(secondSlice, secondNum)
+	}
+	sort.Ints(firstSlice)
+	sort.Ints(secondSlice)
+	return firstSlice, secondSlice
+}
+
+func getDistances(firstSlice []int, secondSlice []int) int {
+	distance := 0
+
+	for index := range len(firstSlice) {
+		distance += int(math.Abs(float64(firstSlice[index]) - float64(secondSlice[index]) ))
+	}
+	return distance
+}
+
+func getSimilarityScore(firstSlice []int, secondSlice []int) int {
+	score := 0
+	freq := map[int]int{}
+
+	for _, num := range secondSlice {
+		freq[num]++
+	}
+
+	for _, num := range firstSlice {
+		score += freq[num] * num
+	}
+	return score
+}
---
+++ 2024/go.mod
@@ -0,0 +1,3 @@
+module github.com/lflor120/advent-of-code/2024
+
+go 1.25.4
