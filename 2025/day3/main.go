3d454d37ea74473802d385d4834603ab7e1759b2
lflor120 <leonard4flores@gmail.com>
2025-12-03 22:11:53 -0600

    feat: part two DONE

--- 2025/day3/main.go
+++ 2025/day3/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"log"
-	"strings"
+	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -15,11 +15,12 @@ func main() {
 		log.Fatalf("failed to open file! %v", err)
 	}
 	banks := getBanks(string(file))
-	// fmt.Println(banks)
 
 	solutionOne := getTotalJoltage(banks)
-	fmt.Printf("Solution one: %d", solutionOne)
+	fmt.Printf("Solution one: %d\n", solutionOne)
 
+	solutionTwo := getTotalNJoltage(banks, 12)
+	fmt.Printf("Solution two: %d", solutionTwo)
 }
 
 func getBanks(fileContents string) [][]int {
@@ -52,10 +53,69 @@ func getLargestJoltage(bank []int) int {
 	return largestJolt
 }
 
+func getLargestNJoltage(bank []int, n int) int64 {
+	jolts := make([]int, 0)
+	// prevIndex := 0
+	want := n
+	total := len(bank)
+	start := 0
+
+	for want > 0 {
+		end := total - (want - 1)
+		if want == 1 {
+			end = total
+		}
+
+		window := bank[start:end]
+		currMax, maxIndex := getLeftMostMax(window) 
+		jolts = append(jolts, currMax)
+
+		start = maxIndex + start + 1      
+		want--                        
+	}
+
+	largestJolt := convertSliceToInt(jolts)
+	return largestJolt
+}
+
+func convertSliceToInt(slice []int) int64 {
+	sliceStr := ""
+	for _, num := range slice {
+		sliceStr += strconv.Itoa(num)
+	}
+
+	sliceInt64, err := strconv.ParseInt(sliceStr, 10, 64)
+	if err != nil {
+		log.Fatalf("failed to convert %v to int! err: %v", sliceStr, err)
+	}
+	return sliceInt64
+}
+
+// returns the num and the index
+func getLeftMostMax(bank []int) (int, int) {
+	leftMax := 0
+	maxIndex := 0
+	for i, num := range bank {
+		if num > leftMax {
+			leftMax = num
+			maxIndex = i
+		}
+	}
+	return leftMax, maxIndex
+}
+
 func getTotalJoltage(banks [][]int) int {
-	joltage := 0
+	var joltage int
 	for _, bank := range banks {
 		joltage += getLargestJoltage(bank)
+	}
+	return joltage
+}
+
+func getTotalNJoltage(banks [][]int, n int) int64 {
+	var joltage int64
+	for _, bank := range banks {
+		joltage += getLargestNJoltage(bank, n)
 	}
 	return joltage
 }
